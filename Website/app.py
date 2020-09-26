04271b187f3a54219031b9b17076f49c7978acbc
Anurag Porel <anurag.porel19@st.niituniversity.in>
2020-09-27 02:11:03 +0530

    Login Page

---
+++ Website/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+app.run(debug=True)
